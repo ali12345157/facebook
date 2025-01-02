d4678dea0122c4f20f27eb6d334eb7a52f523a7d
Ali Ashraf Soliman <aliashraf152003@gmail.com>
2025-01-02 13:29:25 +0200

    like_comment_share2

--- app/src/main/java/com/example/likecomment/MainActivity.java
+++ app/src/main/java/com/example/likecomment/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
 
         recyclerView = findViewById(R.id.recyclerview);
         createSettings();
